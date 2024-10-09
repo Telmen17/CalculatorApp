168d0f6e49711d7a3de4cf3218f46943a7b739d5
Telmen17 <telmen7948@gmail.com>
2024-10-09 13:36:57 -0700

    Added JavaDoc and supplementary comments.

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -1,3 +1,7 @@
+/**
+ * @author: Telmen Enkhtuvshin
+ * The MainActivity class which controls the logical backend processes of the Calculator App.
+ */
 package com.example.calculatorapp;
 
 import android.os.Bundle;
@@ -15,16 +19,21 @@ import com.google.android.material.button.MaterialButton;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /**
+     * Text fields and buttons of the calculator app.
+     */
     TextView resultTV, solutionTV;
     MaterialButton buttonC, button_open_bracket, button_closed_bracket;
     MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
     MaterialButton buttonMul, buttonPlus, buttonSub, buttonDivide, buttonEquals;
     MaterialButton buttonAC, buttonDot;
     /**
-     * Initializes the application window
-     * @param savedInstanceState The state of the window
+     * Initializes the application window and assigns the
+     * text and button elements to the field objects.
+     * @param savedInstanceState The state of the display window.
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +46,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return insets;
         });
 
+        //Assigning the IDs of TextView elements to their objects.
         resultTV = findViewById(R.id.result_tv);
         solutionTV = findViewById(R.id.solution_tv);
 
+        //Assigning the IDs of MaterialButton elements to their objects.
         assignID(buttonC, R.id.button_c);
         assignID(button_open_bracket, R.id.button_open_bracket);
         assignID(button_closed_bracket, R.id.button_closed_bracket);
@@ -63,58 +74,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     /**
-     *
-     * @param btn
-     * @param id
+     * A method to assign the button elements in the window to the field objects for initialization.
+     * @param btn MaterialButton class button.
+     * @param id Unique integer id of the button.
      */
-    void assignID(MaterialButton btn, int id) {
+    private void assignID(MaterialButton btn, int id) {
         btn = findViewById(id);
         btn.setOnClickListener(this);
     }
 
     /**
-     *
-     * @param view
+     * Action listener onClick method that detects a button press on the display screen.
+     * If an action is detected, the app reacts accordingly.
+     * All of the user interactivity of inputs, calculations, and output display are handled here.
+     * @param view View class object that is the whole screen of the app.
      */
     @Override
     public void onClick(View view) {
+        //Declaring the variables to be used.
         MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
         String dataToCalculate = solutionTV.getText().toString();
 
+        //If the "AC" button is pressed, the two text fields gets reset.
         if (buttonText.equals("AC")) {
             solutionTV.setText("");
             resultTV.setText("0");
             return;
         }
+        //If the "=" button is pressed, the data is calculated and displayed on the
+        //result text field.
         if (buttonText.equals("=")) {
-            //solutionTV.setText(resultTV.getText());
             String finalResult = getResults(dataToCalculate);
             resultTV.setText(finalResult);
-//            if (!finalResult.equals("Error")) {
-//                resultTV.setText(finalResult);
-//            } else {
-//                resultTV.setText(finalResult);
-//            }
-
             return;
         }
+        //If the "C" button is pressed, then the solution text field's last value is deleted.
         if (buttonText.equals("C")) {
             dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
         } else {
+            //Else, the dataToCalculate is  concatenated with the pressed button text.
             dataToCalculate = dataToCalculate + buttonText;
         }
 
+        //The solutionTV text field is updated with a new String value.
         solutionTV.setText(dataToCalculate);
     }
 
-    String getResults(String data) {
+    /**
+     * A helper method that calculates the given arithmetic string data.
+     * @param data String type arithmetic data to be calculated.
+     * @return Returns a String type number to be displayed on the user screen interface.
+     */
+    private String getResults(String data) {
+        //The arithmetic data is calculated.
         try {
             Context context = Context.enter();
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
+            //String number is returned.
             return context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
         } catch (Exception e) {
+            //If there is an exception, return the String "Error".
             return "Error";
         }
     }
